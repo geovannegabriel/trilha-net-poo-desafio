f9877b4a606a07e2f00a03f5d9fbe86944216874
geovannegabriel <geovannegabriel29@hotmail.com>
2025-06-16 21:38:54 -0300

    Aletrado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Feito: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Feito: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Intalando '{nomeApp}' no Iphone {Modelo} com Imei {Imei}.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo Novo", imei: "123654", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("ZapZap");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "163956", modelo: "Modelo Mais Novo", imei: "128874", memoria: 32);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Youtube");
+    }
+}
